0b49181379139192e7afb7ec7c9b9d05521ad0ae
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-03-02 15:02:59 -0600

    Lifebar created and connected to homeview

--- app/src/main/java/com/example/rexproject/home/ui/view/HomeView.kt
+++ app/src/main/java/com/example/rexproject/home/ui/view/HomeView.kt
@@ -1,15 +1,14 @@
 package com.example.rexproject.home.ui.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,6 +19,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.rexproject.R
+import com.example.rexproject.shared.ui.LifeBar
 
 @Composable
 fun HomeView(
@@ -28,6 +28,20 @@ fun HomeView(
 ) {
     Scaffold(
         containerColor = MaterialTheme.colorScheme.primaryContainer,
+        topBar = {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = modifier
+                    .padding(top = 20.dp)
+                    .fillMaxWidth()
+            ){
+                LifeBar(percentage = 100, percentageTitle = "HP")
+                LifeBar(percentage = 60, percentageTitle = "COMIDA")
+                LifeBar(percentage = 40, percentageTitle = "CORDURA")
+                LifeBar(percentage = 30, percentageTitle = "LIMPIEZA")
+            }
+        }
     ){
         Column(
             modifier = modifier
--- app/src/main/java/com/example/rexproject/shared/ui/LifeBar.kt
+++ app/src/main/java/com/example/rexproject/shared/ui/LifeBar.kt
@@ -1,9 +1,59 @@
 package com.example.rexproject.shared.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun LifeBar(modifier: Modifier = Modifier) {
-    
+fun LifeBar(percentageTitle : String, percentage : Int) {
+    val barColor = when {
+        percentage > 70 -> Color.Green
+        percentage in 40..70 -> Color.Yellow
+        else -> Color.Red
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(7.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(20.dp)
+                .width(400.dp)
+                .background(
+                    Color.Gray,
+                    RoundedCornerShape(10.dp)
+                )
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width((400 * percentage / 100).dp)
+                    .background(barColor,  RoundedCornerShape(10.dp))
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+
+                modifier = Modifier.padding( horizontal = 10.dp).fillMaxWidth()
+            ){
+                Text(text = percentageTitle, fontSize = 15.sp)
+                Text(text = "$percentage%", fontSize = 18.sp)
+            }
+        }
+    }
 }
