f224a16a1395b2fa930bec35e832b8ef537b58b7
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-03-05 21:43:43 -0600

    network security updated

--- app/src/main/java/com/example/rexproject/core/domain/constants/ApiConstants.kt
+++ app/src/main/java/com/example/rexproject/core/domain/constants/ApiConstants.kt
@@ -1,7 +1,7 @@
 package com.example.rexproject.core.domain.constants
 
 object ApiConstants {
-    const val API_BASE_URL = "http://127.0.0.1:8080"
+    const val API_BASE_URL = "http://54.158.219.46:8080"
     const val USER_BASE_URL = "$API_BASE_URL/user"
     const val DINOPET_BASE_URL = "$API_BASE_URL/dinopet"
 }
